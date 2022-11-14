07761f00e9f502e42218d5be2727577d54d310b2
Russell Gordon <rgordon@lcs.on.ca>
2022-11-14 09:34:34 -0500

    Initial Commit

---
+++ ShortestNameWins/ShortestNameWinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortestNameWinsApp.swift
+//  ShortestNameWins
+//
+//  Created by Russell Gordon on 2022-11-14.
+//
+
+import SwiftUI
+
+@main
+struct ShortestNameWinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
